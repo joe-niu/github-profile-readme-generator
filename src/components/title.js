536c1ea011fc113a4c7158ddd297f900c6ae1281
joe-niu <joeniu@my.unt.edu>
2021-03-15 13:19:10 -0500

    Added 2 bugs to title.js

--- src/components/title.js
+++ src/components/title.js
@@ -11,14 +11,14 @@ const Title = props => {
           id="title-prefix"
           className="outline-none w-24 sm:w-40 mr-10 border-t-0 border-l-0 border-r-0 border solid border-gray-900 py-1 px-2 focus:border-blue-700 prefix"
           value={props.prefix.title}
-          onChange={event => props.handlePrefixChange("title", event)}
+          //onChange={event => props.handlePrefixChange("title", event)}
         />
         <input
           id="title-name"
           placeholder="name"
           className="outline-none placeholder-gray-700 w-1/2 sm:w-1/3 border-t-0 border-l-0 border-r-0 border solid border-gray-900 py-1 px-2 focus:border-blue-700"
           value={props.data.title}
-          onChange={event => props.handleDataChange("title", event)}
+          //onChange={event => props.handleDataChange("title", event)}
         />
       </div>
     </div>
